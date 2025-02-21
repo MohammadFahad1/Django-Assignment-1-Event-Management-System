1addb47f6b240397e50b5e398ea780feddc3fa0c
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-02-21 20:39:48 +0600

    Form problems solved

--- events/views.py
+++ events/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from events.forms import CategoryModelForm
 
 def home_page(request):
     form = CategoryModelForm()
+    if request.method == 'POST':
+        print(request.POST)
     return render(request, 'category_form.html', {"form": form})
