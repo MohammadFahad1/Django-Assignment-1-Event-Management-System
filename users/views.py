fc4f0fb81649b7ca89a7c0afea3d7f0259e7eb7a
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-04-08 23:56:45 +0600

    Preparing to update the signup page

--- users/views.py
+++ users/views.py
@@ -4,10 +4,10 @@ from django.contrib import messages
 
 # Create your views here.
 def sign_up(request):
-    form = UserCreationForm()
+    form = UserCreationForm() # Default form provided by Django
     
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(request.POST) # Default form provided by Django
         if form.is_valid():
             form.save()
             messages.success(request, "Account created successfully")
