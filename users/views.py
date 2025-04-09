9e791cd8d752b0c4549e346420fa2f4346fdbe10
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-04-09 23:41:12 +0600

    Registration form customized

--- users/views.py
+++ users/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.models import User
+
+from users.forms import RegisterForm
 
 # Create your views here.
 def sign_up(request):
-    form = UserCreationForm() # Default form provided by Django
+    form = RegisterForm() # Default form provided by Django
     
     if request.method == 'POST':
-        form = UserCreationForm(request.POST) # Default form provided by Django
+        form = RegisterForm(request.POST) # Default form provided by Django
         if form.is_valid():
             form.save()
-            messages.success(request, "Account created successfully")
-            return redirect('sign-up')
+            messages.success(request, 'User created successfully')
+        #     username = form.cleaned_data('username')
+        #     password = form.cleaned_data('password1')
+        #     confirm_password = form.cleaned_data('password2')
+
+        #     if password and confirm_password and password == confirm_password:
+        #         User.objects.create(username=username, password=password)
+        #         messages(request, 'User created successfully')
+        #     else:
+        #         messages.error(request, 'Password does not match')
+        # else:
+        #     messages.message(request, 'Error creating user')
         
     return render(request, 'registration/register.html', {"form": form})
