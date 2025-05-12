ffbec4eb083fe28b353a8a3bec7e6b13fedc4e1f
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-05-12 22:34:45 +0600

    Email configuaration done

--- event_management/settings.py
+++ event_management/settings.py
@@ -165,5 +165,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '36fahad@gmail.com'
-EMAIL_HOST_PASSWORD = 'vvnu dbjg gifr hbag'
+EMAIL_HOST_USER = 'fahad4bangladesh@gmail.com'
+EMAIL_HOST_PASSWORD = 'jfml pajw iuoy ldyl'
