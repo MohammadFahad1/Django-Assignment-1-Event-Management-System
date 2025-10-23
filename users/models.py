1375527c05bc7bf7a26b5346cd2f91fd337c381c
MohammadFahad1 <fahad4bangladesh@gmail.com>
2025-10-23 22:31:49 +0600

    minor issue fixed with users model

--- users/models.py
+++ users/models.py
@@ -5,6 +5,3 @@ class CustomUser(AbstractUser):
     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True, default='profile_images/default_profile.png')
     phone = models.TextField(null=True, blank=True)
     location = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return self.username
